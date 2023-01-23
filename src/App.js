b5769c0b60f93a867cf3d9f07709a0aa86cf4c5b
Daniel Mize <dmize@umd.edu>
2023-01-23 17:31:09 -0500

    trying to config tailwind

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
@@ -8,7 +9,7 @@ import Testimonials from './components/Testimonials';
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+      <Navbar class/>
       <About />
       <Projects />
       <Skills />
